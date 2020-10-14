9bf26ea6635a8c5820d34ac50adae0547727d6ea
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-10-14 11:19:43 -0300

    adicionando tratamento de error 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,10 +1,13 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor (public storage : StorageService){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
@@ -23,9 +26,19 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log("Error detectado pelo interceptor");
             console.log(errorObj);
 
+            switch(errorObj.status) {
+                case 403:
+                    this.handle403();
+                    break;
+            }
+
             return Observable.throw(errorObj);
             
         }) as any;
+    }
+
+    handle403 () {
+        this.storage.setLocalUser(null);
     }
 }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -21,13 +21,20 @@ export class ProfilePage {
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
+
     if(localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.cliente = response;
           this.getImageIfExist();
         }, 
-        error => {});
+        error => {
+          if(error.status == 403){
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
