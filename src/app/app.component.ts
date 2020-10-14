55fa8ccefbcb84cd6175075471cac532dc5fe151
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-10-14 13:04:18 -0300

    criando logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, NavController, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService } from '../services/auth.service';
 
 
 @Component({
@@ -14,13 +15,14 @@ export class MyApp {
 
   pages: Array<{title: string, component: string}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public authServie: AuthService) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Perfil', component: 'ProfilePage' },
-      { title: 'Categorias', component: 'CategoriasPage' }
+      { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Logout', component: '' },
     ];
 
   }
@@ -34,9 +36,16 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  openPage(page : {title:string, component:string}) {
+
+    switch (page.title) {
+      case "Logout":
+        this.authServie.logout();
+        this.nav.setRoot('HomePage');
+        break;
+      default:
+        this.nav.setRoot(page.component);
+    }
+    
   }
 }
