719415e5fc7c3382a228458bcaeb9549e62c547a
wesley arcanjo <wesleyferreirarcanjo@gmail.com>
2020-10-16 12:33:41 -0300

    adicionando img do produto

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
-/**
- * Generated class for the ProdutosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -19,15 +14,35 @@ export class ProdutosPage {
 
   items: ProdutoDTO[];
 
+  bucketUrl = API_CONFIG.bucketBaseUrl + "prod"; 
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
-
     this.produtoService.findByCategoria(this.navParams.get("id"))
       .subscribe(response => {
         this.items = response['content'];
+        this.getImageIfExist();
       }, error => {});
   }
+
+  getImageIfExist() {
+
+    for (let i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+      .subscribe(reponse => {
+        item.imageUrl = `${API_CONFIG.bucketBaseUrl}prod${item.id}-small.jpg`;
+      },
+      error => {})
+    }
+      
+    }
+
+
+
+
+
 
 }
